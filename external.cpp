1de7e737c0e60da051eb4befa4e3704f0842837d
Luke <lwegryn@vt.edu>
2014-10-14 16:24:15 -0400

    POSIX Queue Trial Working

--- external.cpp
+++ external.cpp
@@ -1,6 +1,16 @@
 #include "QStream.h"
 #include <QList>
 #include <QVariant>
+#include <mqueue.h>
+#include <stdio.h>
+
+#define P0 "/70"
+#define P1 "/71"
+#define P2 "/72"
+#define P3 "/73"
+#define P4 "/74"
+#define P5 "/75"
+#define P6 "/76"
 
 QList<QString> parseArguments(int argc, char *argv[])
 {
@@ -37,8 +47,42 @@ bool getArgumentValues(QList<QString> arguments,int &pid, float &initialTemperat
 
 int main(int argc, char *argv[])
 {
-	int pid = -1;
-	float initialTemperature;
-	if(!getArgumentValues(parseArguments(argc, argv), pid, initialTemperature)) exit(0);
+	int pid = -1; //PID 
+	float temp; //inital temperature from command line
+	//parses command line arguments and updates pid and initial temperature/exits on fail
+	if(!getArgumentValues(parseArguments(argc, argv), pid, temp)) exit(0);
+	
+	mqd_t mailbox; /*message queue*/
+	mqd_t parent;
+	mqd_t child_1;
+	mqd_t child_2;
+	struct mq_attr ma;
+	ma.mq_flags = 0;
+	ma.mq_maxmsg = 10;
+	ma.mq_msgsize = 33;
+	ma.mq_curmsgs = 0;
+
+	/*char send[sizeof(float)];
+	char receive[40];
+	float *f_receive = (float*)receive;
+	f_receive = 5.0;*/
+	char send[40];
+	float *f_send = (float*)send;
+	*f_send = temp;
+	char receive[40];
+	float *f_receive = (float*)receive;
+
+	switch(pid){
+		case 0:
+			mailbox = mq_open(P0, O_CREAT|O_RDWR, 0644, &ma);
+			mq_send(mailbox, send, sizeof(float)+1, 1);
+			//qout << ma.mq_curmsgs;
+			mq_receive(mailbox, receive, 50, NULL);
+			mq_close(mailbox);
+			mq_unlink(P0);
+			qout << f_receive[0];
+			break;
+		
+	}
 	return 0;
 }
